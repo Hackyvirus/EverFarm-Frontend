941f1e3168156b8d622d902085d2c508d4a44ab4
sushantatram <hackyvirus591081@gmail.com>
2024-07-31 06:14:08 +0530

    done 1

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,16 @@
 const nextConfig = {
     async rewrites() {
         return [
-            
             {
                 source: '/auth/:path*',
-                destination: 'https://ever-farm-backend-g57m-p9i4m9r9n-sushants-projects-c40f823c.vercel.app:/auth/:path*', // This should match the backend's API path
+                destination: 'https://ever-farm-backend-ndg5-1iqisgun1-sushants-projects-c40f823c.vercel.app/auth/:path*',
             },
             {
                 source: '/api/:path*',
-                destination: 'https://ever-farm-backend-g57m-p9i4m9r9n-sushants-projects-c40f823c.vercel.app/api/:path*', // This should match the backend's API path
+                destination: 'https://ever-farm-backend-ndg5-1iqisgun1-sushants-projects-c40f823c.vercel.app/api/:path*', 
             }
         ];
+        ;
     }, 
 };
 
